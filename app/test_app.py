373d99c04df0c742000c1cd9add16fb53bc31d79
Sara-AprendizCBA <s.rodriguez.aprendizsena@gmail.com>
2026-09-02 21:17:34 -0500

    fix: actualizar import de app en test_app.py y agregar PYTHONPATH en workflow

--- app/test_app.py
+++ app/test_app.py
@@ -1,5 +1,5 @@
+from app.app import app
 import pytest
-from app.main import app
 
 @pytest.fixture
 def client():
@@ -10,4 +10,3 @@ def client():
 def test_health_check(client):
     response = client.get('/health')
     assert response.status_code == 200
-    assert response.json == {"status": "OK"}
